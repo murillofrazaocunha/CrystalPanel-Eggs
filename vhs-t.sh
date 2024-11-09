341d2e035e0ef8dc4f1ab3aacfe74ea304c9c130
Murillo Frazão Cunha <105169407+murillofrazaocunha@users.noreply.github.com>
2024-11-08 22:24:22 -0300

    Update and rename vhs-s.sh to vhs-t.sh

--- vhs-t.sh
+++ vhs-t.sh
@@ -6,26 +6,8 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# Variável para a porta
 PORTA="$1"
 
-# Solicita a senha do usuário root e confirmação
-while true; do
-  echo "Digite a senha para o usuário root:"
-  read -s ROOT_PASSWORD
-  echo "Confirme a senha para o usuário root:"
-  read -s ROOT_PASSWORD_CONFIRM
-
-  # Verifica se as senhas coincidem
-  if [ "$ROOT_PASSWORD" == "$ROOT_PASSWORD_CONFIRM" ]; then
-    echo -e "\nSenha confirmada com sucesso."
-    break
-  else
-    echo -e "\nAs senhas não coincidem. Tente novamente."
-  fi
-done
-
-# Atualiza o apt e instala o openssh-server
 sudo apt update && sudo apt install -y openssh-server
 
 # Modifica o arquivo de configuração do sshd para usar a porta especificada
@@ -36,9 +18,10 @@ sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 # Define a senha para o usuário root
-echo "root:$ROOT_PASSWORD" | sudo chpasswd
+echo "root:12345678" | sudo chpasswd
 
 # Inicia o serviço SSH
 sudo service ssh start
 
 echo "Configuração concluída e serviço SSH iniciado na porta $PORTA."
+echo "Use ssh root@IP -p $PORTA com a senha 12345678 para acessar o SSH" 

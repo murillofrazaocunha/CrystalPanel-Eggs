0fe1859ccbe96d0a062bb2681361afc7a374d90f
Murillo Frazão Cunha <105169407+murillofrazaocunha@users.noreply.github.com>
2024-11-08 22:44:12 -0300

    Update and rename vhs-z.sh to vhs-a.sh

--- vhs-a.sh
+++ vhs-a.sh
@@ -8,29 +8,28 @@ fi
 
 PORTA="$1"
 
-# Atualiza o apt e instala o servidor SSH
-apt update
-apt install -y openssh-server
-
-# Continua com o script
-echo "Conteúdo original de /etc/ssh/sshd_config:"
-cat /etc/ssh/sshd_config
-
+# Cria o segundo script para configuração do SSH
+cat <<EOF > /tmp/configura_ssh.sh
+#!/bin/bash
 # Modifica o arquivo de configuração do sshd para usar a porta especificada
 sed -i "s/#Port 22/Port $PORTA/" /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
-# Mostra o conteúdo do sshd_config após as alterações
-echo "Conteúdo de /etc/ssh/sshd_config após alterações:"
-cat /etc/ssh/sshd_config
-
 # Define a senha para o usuário root
 echo "root:12345678" | chpasswd
 
 # Inicia o serviço SSH
-service ssh start
+service ssh restart
 
 # Exibe as mensagens de status
 echo "Configuração concluída e serviço SSH iniciado na porta $PORTA." | tee /dev/stderr
 echo "Use ssh root@IP -p $PORTA com a senha 12345678 para acessar o SSH" | tee /dev/stderr
+EOF
+
+# Torna o script executável
+chmod +x /tmp/configura_ssh.sh
+
+# Instala o SSH e chama o script de configuração em segundo plano
+apt update
+apt install -y openssh-server && /tmp/configura_ssh.sh &

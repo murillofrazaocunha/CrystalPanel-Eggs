7e83cd6fbe57b2449efcc3d37300bfb5588ae765
Murillo Frazão Cunha <105169407+murillofrazaocunha@users.noreply.github.com>
2024-11-08 22:21:15 -0300

    Update and rename vhs.sh to vhs-s.sh

--- vhs-s.sh
+++ vhs-s.sh
@@ -9,21 +9,30 @@ fi
 # Variável para a porta
 PORTA="$1"
 
-# Solicita a senha do usuário root
-echo "Digite a senha para o usuário root:"
-read -s ROOT_PASSWORD
+# Solicita a senha do usuário root e confirmação
+while true; do
+  echo "Digite a senha para o usuário root:"
+  read -s ROOT_PASSWORD
+  echo "Confirme a senha para o usuário root:"
+  read -s ROOT_PASSWORD_CONFIRM
+
+  # Verifica se as senhas coincidem
+  if [ "$ROOT_PASSWORD" == "$ROOT_PASSWORD_CONFIRM" ]; then
+    echo -e "\nSenha confirmada com sucesso."
+    break
+  else
+    echo -e "\nAs senhas não coincidem. Tente novamente."
+  fi
+done
 
 # Atualiza o apt e instala o openssh-server
 sudo apt update && sudo apt install -y openssh-server
 
-echo "Setando porta do SSH"
 # Modifica o arquivo de configuração do sshd para usar a porta especificada
 sudo sed -i "s/#Port 22/Port $PORTA/" /etc/ssh/sshd_config
 
 # Permite o login root e autenticação por senha
-echo "Login do ROOT"
 sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-echo "Login por SENHA"
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 # Define a senha para o usuário root

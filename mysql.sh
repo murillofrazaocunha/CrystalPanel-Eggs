c01ddd15ad164883285e4eb1cc5fe8975bda357c
Murillo Frazão Cunha <105169407+murillofrazaocunha@users.noreply.github.com>
2024-11-09 15:56:57 -0300

    Create mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Verificando se os argumentos foram passados corretamente
+if [ $# -ne 2 ]; then
+  echo "Uso: $0 <porta> <jar>"
+  exit 1
+fi
+
+# Atribuindo os argumentos a variáveis
+port=$1
+jar=$2
+
+if [ ! -d "/app/mysql" ]; then
+  mysqld --initialize --datadir=/app
+fi
+
+mysqld --datadir=/app --user=mysql --port=${port} &
+
+until mysqladmin ping --silent -P ${port}; do
+  sleep 2
+done
+
+mysqladmin -u root password "${jar}" -P "${port}"
+mysql -u root -p"${jar}" -P "${port}"

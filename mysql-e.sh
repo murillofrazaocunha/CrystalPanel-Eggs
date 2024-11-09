2ab817059a103e53ac7acba3cc4fb27c39883c26
Murillo Frazão Cunha <105169407+murillofrazaocunha@users.noreply.github.com>
2024-11-09 16:34:47 -0300

    Update mysql-e.sh

--- mysql-e.sh
+++ mysql-e.sh
@@ -14,6 +14,7 @@ jar=$2
 if [ ! -d "/app/mysql" ]; then
   # Inicializa o banco de dados e captura a saída
   init_output=$(mysqld --initialize --datadir=/app 2>&1)
+  echo "$init_output"
   # Captura a senha temporária da saída
   temp_password=$(echo "$init_output" | grep -oP 'A temporary password is generated for root@localhost: \K.*')
 fi
@@ -26,12 +27,12 @@ fi
 
 # Verifica se a senha temporária foi encontrada
 if [ -n "$temp_password" ]; then
-
+  echo "$temp_password"
   # Faz login no MySQL com a senha temporária
-  mysql -u root -p"${temp_password}" -P "${port}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${jar}'"
+  mysql -u root -p"${temp_password}" -P "${port}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$jar'"
 
   # Confirma a alteração da senha
-  mysqladmin -u root -p"${jar}" -P "${port}" ping
+  mysqladmin -u root -p"$jar" -P "$port" ping
 
   echo "Senha do usuário root alterada com sucesso!"
 fi
